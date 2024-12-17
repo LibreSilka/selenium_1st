c48f7f349703f8d115cface375d00b79699a6362
hrobo <nikitos9607@gmail.com>
2024-12-17 09:38:22 +0300

    добавил файл scrolls methods

--- firefox.py
+++ firefox.py
@@ -1,4 +1,5 @@
 import time
+import requests
 import pytest
 from selenium import webdriver
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
@@ -38,6 +39,39 @@ if not os.path.exists("screenshots"):
 #     pict_but = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(("xpath", "//a[text()='Картинки']")))
 #     pict_but.click()
 #     assert driver.current_url == "https://www.google.com/imghp?hl=ru&ogbl"
+
+# def download_image(driver, image_index=0):
+#     # Найти изображения на странице (например, первое изображение)
+#     images = WebDriverWait(driver, 10).until(
+#         EC.presence_of_all_elements_located(("xpath", "// img[ @ id = 'dimg_yg1gZ8fQL-a1wN4Pyt-d-Aw_19']"))
+#     )
+#     print(images)
+#     if images is not None:
+#         # Получить URL изображения
+#         image_url = images.get_attribute("src")
+#         print(f"Image URL: {image_url}")
+#
+#         # Скачать изображение
+#         if image_url:
+#             response = requests.get(image_url, stream=True)
+#             if response.status_code == 200:
+#                 # Создать папку для сохранения, если ее нет
+#                 save_dir = "downloaded_images"
+#                 if not os.path.exists(save_dir):
+#                     os.makedirs(save_dir)
+#
+#                 # Имя файла для сохранения
+#                 file_name = os.path.join(save_dir, f"image_{image_index}.jpg")
+#                 with open(file_name, "wb") as file:
+#                     for chunk in response.iter_content(1024):
+#                         file.write(chunk)
+#                 print(f"Image saved to {file_name}")
+#             else:
+#                 print(f"Failed to download image, status code: {response.status_code}")
+#         else:
+#             print("Image URL not found.")
+#     else:
+#         print("No images found.")
 def test_search_func(driver):
     search_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(("xpath", "//textarea[@class = 'gLFyf']")))
     search_field.send_keys("крокодил")
@@ -45,3 +79,5 @@ def test_search_func(driver):
     search_but = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(("xpath", "//input[@class = 'gNO89b']")))
     search_but.click()
     time.sleep(3)
+    pics_but = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(("xpath", "//div[text() = 'Картинки']")))
+    pics_but.click()
